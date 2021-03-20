465e1361ceb5b795f9c0ab5e33a586b0189a986a
vitkocapcap <viet.npb2212@gmail.com>
2021-03-20 21:16:43 +0100

    small fix

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Rank = ({name, entries})=>{
-    return(
-        <div>
-            <div className='white f3 '>
-                {`Come on ${name}, your current entry is...`}
-            </div>
-            <div className='white f1 '>
-                {entries}
-            </div>
-        </div>
-    );
-}
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className="white f3 ">{`Hello ${name}, give this a try...`}</div>
+      <div className="white f1 ">{entries}</div>
+    </div>
+  );
+};
 
 export default Rank;
